83fbb691c27730930621481b7fb2215a6a808874
Mahdi Amini <mahdiaminik@gmail.com>
2022-06-22 13:19:43 +0000

    fix decimal leading zeros

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,6 +1,6 @@
 set -x
 sshpass -p $SSH_PASSWORD ssh -N \
-    -p $((RAHA_ST+20000)) \
+    -p $((10#$RAHA_ST + 20000)) \
     -o "StrictHostKeyChecking no" \
     -o ServerAliveInterval=60 \
     -R 0.0.0.0:222:host.docker.internal:22 \
